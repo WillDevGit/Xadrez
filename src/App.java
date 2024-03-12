26b25da7bdd5bc62e42c742620d135758dc48d39
Willian Silva <williansilvaws063@gmail.com>
2024-03-12 18:20:21 -0300

    Adiciona novas classes e imprime tabuleiro

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import boardgame.*; 
+import chess.*; 
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Position p = new Position(8, 8);
+        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch(); 
+        UI.printBoard(chessMatch.getPieces());
     }
 }
