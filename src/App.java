fa7695f582d30ecf22c60622c8b11e9e607643af
Willian Silva <williansilvaws063@gmail.com>
2024-03-13 11:08:51 -0300

    Adiciona peças no tabueiro

--- src/App.java
+++ src/App.java
@@ -1,12 +1,13 @@
+import javax.swing.JFrame;
+
 import boardgame.*; 
 import chess.*; 
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        //Position p = new Position(8, 8);
-        Board board = new Board(8, 8);
-        ChessMatch chessMatch = new ChessMatch(); 
+        ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
+      
     }
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -14,13 +14,5 @@ public class Piece {
     protected Board getBoard() {
         return board;
     }
-    
-    
-
-    
-
-    
-
-
 
 }
