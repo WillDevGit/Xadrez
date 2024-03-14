e70e96b5216cd8f8883ee027a8e17fb10be9b250
Willian Silva <williansilvaws063@gmail.com>
2024-03-14 17:01:10 -0300

    Implementa possíveis movimentos

--- src/App.java
+++ src/App.java
@@ -1,13 +1,39 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import javax.swing.JFrame;
 
 import boardgame.*; 
 import chess.*; 
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-        ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
-      
-    }
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
+	}
 }
